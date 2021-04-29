5512b74ff8bfe252f9329c3f312a2eefd395d820
Agnes <agnesfo@kth.se>
2021-04-29 13:30:30 +0200

    update

--- counter/src/App.js
+++ counter/src/App.js
@@ -8,12 +8,12 @@ function App() {
   return (
     <div className="App">
         <h1>COUNTER</h1>
-        <div style={{height:'100%', padding:'10%'}} >
-          <button style={{height:'250px', width:'250px', fontSize:'150px'}}  onClick={() => setNum(num - 1)} >-</button>
-          <p style={{fontSize:'250px', display:'inline-block', margin:'50px'}} > {num} </p> 
-          <button style={{height:'250px', width:'250px', fontSize:'150px'}} onClick={() => setNum(num + 1)} >+</button>
+        <div id="wrapper"  >
+          <button onClick={() => setNum(num - 1)} >-</button>
+          <p id="number" > {num} </p> 
+          <button onClick={() => setNum(num + 1)} >+</button>
         </div>
-        {(num < 50) ? '' : 'I LOVE DEVOPS'}
+        <div id="madeBy"></div>
     </div>
   );
 }
