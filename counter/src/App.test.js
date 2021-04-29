5495444f09055a142b7f9c89f83b2de1158191e8
Agnes <agnesfo@kth.se>
2021-04-29 17:18:37 +0200

    fix branch issue

--- counter/src/App.test.js
+++ counter/src/App.test.js
@@ -1,31 +1,3 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import App from './App'
 
-test('test 2 buttons render', () => {
-  render(<App />)
-  const buttons = screen.getAllByRole('button')
-  expect(buttons).toHaveLength(2)
-})
-
-test('test number plus 1', () => {
-    render(<App/>)
-    const plusButton = screen.getByRole('button', {name: '+'})
-    const number = screen.getByText('1')
-    fireEvent(plusButton, new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-      }))
-    expect(number.textContent).toBe('2')
-})
-
-test('test number plus 1', () => {
-    render(<App/>)
-    const minusButton = screen.getByRole('button', {name: '-'})
-    const number = screen.getByText('1')
-    fireEvent(minusButton, new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-      }))
-    expect(number.textContent).toBe('0')
-})
-
